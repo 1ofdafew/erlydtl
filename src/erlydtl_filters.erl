ea0a5ef853381fc98cd91d5be9f5bf4af571f5ba
Da Few <1ofdafew@gmail.com>
2017-02-07 23:22:53 +0800

    Fixed the satoshi numbers

--- src/erlydtl_filters.erl
+++ src/erlydtl_filters.erl
@@ -577,7 +577,7 @@ rjust(Input, Number) ->
     string:right(Input, Number).
 
 satoshi(Input) ->
-    stringformat(Input/10000000, ".8f").
+    stringformat(Input/100000000, ".8f").
 
 %% @doc Returns a slice of the list.
 slice(Input, Index) when is_binary(Input) ->
